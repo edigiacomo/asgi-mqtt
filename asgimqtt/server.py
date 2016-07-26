b866967f1464d44c1c0b7c9d88e3675f830851cd
Emanuele Di Giacomo <emanuele.digiacomo@gmail.com>
2016-07-26 17:16:33 +0200

    Reconnecting

--- asgimqtt/server.py
+++ asgimqtt/server.py
@@ -1,4 +1,5 @@
 import logging
+import time
 
 import paho.mqtt.client as mqtt
 
@@ -9,6 +10,26 @@ logger = logging.getLogger(__name__)
 def on_connect(client, userdata, rc):
     logger.info("Connected with status {}".format(rc))
     client.subscribe("#")
+
+
+def on_disconnect(client, userdata, rc):
+    server = userdata["server"]
+    logger.info("Disconnected")
+    if not server.stop:
+        j = 3
+        for i in range(j):
+            logger.info("Trying to reconnect")
+            try:
+                client.reconnect()
+                logger.info("Reconnected")
+                break
+            except Exception as e:
+                if i < j:
+                    logger.warn(e)
+                    time.sleep(1)
+                    continue
+                else:
+                    raise
 
 
 def on_message(client, userdata, message):
@@ -29,13 +50,21 @@ class Server(object):
         self.host = host
         self.port = port
         self.client = mqtt.Client(userdata={
+            "server": self,
             "channel": self.channel,
             "host": self.host,
             "port": self.port,
         })
         self.client.on_connect = on_connect
+        self.client.on_disconnect = on_disconnect
         self.client.on_message = on_message
 
     def run(self):
+        self.stop = False
         self.client.connect(self.host, self.port)
-        self.client.loop_forever()
+        logger.info("Starting loop")
+        while not self.stop:
+            self.client.loop()
+            logger.debug("Restarting loop")
+
+        self.client.disconnect()

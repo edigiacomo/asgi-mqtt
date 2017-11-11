5250d5f78175f51a6b33200ff10f9220eaf905f7
Emanuele Di Giacomo <emanuele@digiacomo.cc>
2017-11-11 14:47:47 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
+import os.path
+import re
 from setuptools import setup
-from asgimqtt import __version__
+
+
+def get_version(package):
+    init_py = open(os.path.join(package, '__init__.py')).read()
+    return re.search("^__version__ = ['\"]([^'\"]+)['\"]", init_py).group(1)
+
 
 setup(
     name="asgimqtt",
-    version=__version__,
+    version=get_version("asgimqtt"),
     author="Emanuele Di Giacomo",
-    author_email="emanuele.digiacomo@gmail.com",
+    author_email="emanuele@digiacomo.cc",
+    url="https://github.com/edigiacomo/asgi-mqtt",
     description="Interface between MQTT broker and ASGI",
     long_description=open("README.rst").read(),
     license="GPLv2+",
@@ -18,4 +26,19 @@ setup(
             "asgimqtt=asgimqtt.cli:main",
         ]
     },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)',
+        'Framework :: Django',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet :: WWW/HTTP',
+    ]
 )
